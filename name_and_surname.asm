380601777b7605f3c662955dc3e154732f002361
TamannaJaiswal <tamannajaiswal347@gmail.com>
2025-07-30 16:27:15 +0530

    Added hello.asm and name_and_surname.asm files

---
+++ name_and_surname.asm
@@ -0,0 +1,20 @@
+global _start
+section .data
+   name db "Tamanna",10;
+   name_len equ $-name
+   surname db "Jaiswal",10;
+   surname_len equ $-surname
+section .text
+%macro print 2
+   mov eax,4
+   mov ebx,2
+   mov ecx,%1
+   mov edx,%2
+   int 80h
+%endmacro
+_start:
+  print name,name_len
+  print surname,surname_len
+  xor ebx,ebx
+  mov eax,1
+  int 80h
